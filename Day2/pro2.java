02097b11e7c4906f58e77cee139ba627b47bb603
Yashwanth-MS <yashwanthms2006s@gmail.com>
2026-08-18 11:40:29 +0530

    Add pro2 class with method to print values

---
+++ Day2/pro2.java
@@ -0,0 +1,14 @@
+package day2;
+public class pro2 {
+	int a = 20;
+
+	void m1(int b) {
+		System.out.println("dfasdf "+b);
+		System.out.println("dfasdf "+a);
+	}
+
+	public static void main(String[] args) {
+		pro2   vv = new pro2();
+		vv.m1(4);
+	}
+}
