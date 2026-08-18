7d594afbe0a16a45b108d187c95121d0d812ff56
Yashwanth-MS <yashwanthms2006s@gmail.com>
2026-08-18 13:21:42 +0530

    Add pro8 class with constructors and main method

---
+++ Day2/pro8.java
@@ -0,0 +1,15 @@
+package day2;
+public class pro8 {
+public pro8() {
+System.out.println("Hello world");
+} 
+public pro8(int a) {
+	System.out.println("Hello world contr");
+} 
+	
+public static void main(String[] args) {
+pro8 gg = new pro8();
+pro8 gf = new pro8(3);
+
+}
+}
