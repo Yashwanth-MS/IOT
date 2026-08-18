1cd3747435ddc5f2b688baf88853a07f5e12320f
Yashwanth-MS <yashwanthms2006s@gmail.com>
2026-08-18 11:42:59 +0530

    Add pro4 class with method m1 and main method

---
+++ Day2/pro4.java
@@ -0,0 +1,17 @@
+package day2;
+public class pro4 {
+	int a = 20;
+	int b = 10;
+
+	void m1(int a,int b ) {
+		System.out.println("asdfas"+(this.a+this.b));
+
+		System.out.println("dfasdf "+(a+b));
+//		System.out.println("dfasdf "+(c+d));
+	}
+
+	public static void main(String[] args) {
+		pro4   vv = new pro4();
+		vv.m1(4,3);
+	}
+}
