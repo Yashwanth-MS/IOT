e315655dc7482e4f7b61f1a2efb3c643632c3096
Yashwanth-MS <yashwanthms2006s@gmail.com>
2026-08-18 11:05:28 +0530

    Implement method overloading in demo class

---
+++ Day2/demo.java
@@ -0,0 +1,15 @@
+package day2;
+//method overloading
+public class demo {
+	void m1() {
+		System.out.println("sdfa");
+	}
+	void m1(int a) {
+		System.out.println("dfasdfasdfasfas");
+	}
+	public static void main(String[] args) {
+		demo test = new demo();// object
+		test.m1();
+		test.m1(3);
+	}
+}
